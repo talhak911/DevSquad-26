a770a8eaa22a5e0071a01c2c603db927f6dfaae4
Talha Khan <talhakhan8300@gmail.com>
2026-03-31 11:18:49 +0500

    feat: initialize backend project structure and configure frontend build environment

--- day-19-ott-platform-hackathon/backend/dist/config/roles.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.roleRights = exports.roles = void 0;
-const allRoles = {
-    user: ['getMovies', 'getProfile', 'manageSubscription'],
-    admin: ['getUsers', 'manageUsers', 'getMovies', 'manageMovies', 'managePlans', 'getProfile', 'manageSubscription'],
-};
-exports.roles = Object.keys(allRoles);
-exports.roleRights = new Map(Object.entries(allRoles));
