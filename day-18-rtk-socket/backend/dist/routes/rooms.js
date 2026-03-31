36106daeb84ace59b35a686edd6b3da2cb247d7e
Talha Khan <talhakhan8300@gmail.com>
2026-03-31 11:18:49 +0500

    feat: initialize backend project structure and configure frontend build environment

--- day-18-rtk-socket/backend/dist/routes/rooms.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const store_1 = require("../store");
-const router = (0, express_1.Router)();
-router.get("/", (_req, res) => {
-    res.json(store_1.rooms);
-});
-exports.default = router;
